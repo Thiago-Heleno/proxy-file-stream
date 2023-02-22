13d9e5fb57f32404bf6872017216c6a7437c0f16
Thiago-Heleno <tibis0313@gmail.com>
2023-02-22 12:31:36 -0600

    a

---
+++ index.cjs
@@ -0,0 +1,31 @@
+var fetch = require('node-fetch')
+var express = require('express')
+var request = require('request')
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+
+
+async function claudio(Id){
+		const dd = await fetch(`https://fembed9hd.com/api/source/${id}`, {method: "POST", referrer: `https://fembed9hd.com/v/${id}`})
+		const cc = await fetch((await dd.json()).data[2].file, {method: "GET", referrer: `https://fembed9hd.com/v/${id}`})
+		
+		const jj = await cc.url
+		if(jj){
+			return jj
+		}else{
+			return
+		}
+	}
+
+let target;
+
+app.use(express.static('dist'));
+
+app.get('/api/:Id', async(req, res) => {
+ target = claudio(req.params.Id)
+  req.pipe(request.get(await target)).pipe(res);
+});
+
+app.listen(PORT);
